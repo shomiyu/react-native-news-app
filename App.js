4e42439146c82cffd168d752e0c04d1eafc995e1
miyuki shoji <0428.miyuki.shoji@gmail.com>
2020-05-03 17:47:00 +0900

    ✨更新　ニュースの一覧表示

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem'
+import articles from './dummies/articles'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListItem title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." imageUrl="https://picsum.photos/200/300" auther="SampleNews" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => <ListItem
+          imageUrl={item.urlToImage}
+          title={item.title}
+          auther={item.auther}
+        />}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +23,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
