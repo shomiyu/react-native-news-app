97237aae3a3054e00984b1867449943d89633a32
miyuki shoji <0428.miyuki.shoji@gmail.com>
2020-05-04 23:43:20 +0900

    🎉追加　axiosの導入とAPIの取得

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
 import ListItem from './components/ListItem'
-import articles from './dummies/articles'
+import dummyArticles from './dummies/articles'
+import Constants from 'expo-constants'
+import axios from 'axios'
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles)
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
