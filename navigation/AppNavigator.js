a4ab2296d437e5fedc13021d608f3a2cc1c489ec
miyuki shoji <0428.miyuki.shoji@gmail.com>
2020-05-05 23:16:12 +0900

    ✨更新　タブメニュー追加

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -5,6 +5,7 @@ import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 import ClipScreen from '../screens/ClipScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -26,10 +27,25 @@ const ClipStack = () => {
   );
 };
 
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === 'Home') {
+      iconName = "home";
+    } else if (route.name === 'Clip') {
+      iconName = "bookmark";
+    }
+
+    // You can return any component that you like here!
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  },
+});
+
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
         <Tab.Screen name="Home" component={HomeStack} />
         <Tab.Screen name="Clip" component={ClipStack} />
       </Tab.Navigator>
