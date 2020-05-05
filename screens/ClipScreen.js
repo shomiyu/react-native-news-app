0f505b2d6bbabf9ba95865c11b78f932eb4f728e
miyuki shoji <0428.miyuki.shoji@gmail.com>
2020-05-05 23:06:51 +0900

    ✨更新　タブの追加

---
+++ screens/ClipScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
+
+export default ClipScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Clip Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
