53179ec1d438e62807073507160c2899bb969328
miyuki shoji <0428.miyuki.shoji@gmail.com>
2020-05-06 14:35:01 +0900

    🎉追加　クリップボタンのオンオフ追加

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addClip, deleteClip } from '../store/actions/user';
-
+// import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+import ClipButton from '../components/ClipButton';
 
 export default ArticleScreen = ({ route }) => {
   const { article } = route.params;
 
   const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+  const { clips } = user;
+
+  const isClipped = () => {
+    return clips.some(clip => clip.url === article.url);
+  };
+
+  const toggleClip = () => {
+    if (isClipped()) {
+      dispatch(deleteClip({ clip: article }));
+    } else {
+      dispatch(addClip({ clip: article }));
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={() => { dispatch(addClip({ clip: article })) }}>
-        <Text style={{ margin: 10, fontSize: 30 }}>Add Clip</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => { dispatch(deleteClip({ clip: article })) }}>
-        <Text style={{ margin: 10, fontSize: 30 }}>Delete Clip</Text>
-      </TouchableOpacity>
+      <ClipButton onPress={toggleClip} enabled={isClipped()} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
